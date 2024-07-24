b8a5a17f90dadda9c3b50d733c8dfa1b9d2aee96
ndkhoa1195 <ndkhoa1195@gmail.com>
2024-07-24 10:20:35 +0700

    finish project

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,10 @@ module.exports = {
         'very-dark-blue-main': 'hsl(217, 54%, 11%)',
         'very-dark-blue-card': 'hsl(216, 50%, 16%)',
         'very-dark-blue-line': 'hsl(215, 32%, 27%)',
+        background: 'hsla(178, 100%, 50%, 0.5)',
+      },
+      boxShadow: {
+        custom: '0px 25px 50px 0px rgba(0, 0, 0, 0.10)',
       },
     },
   },
